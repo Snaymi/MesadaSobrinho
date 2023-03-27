410f0146637476c0a0c8cee736ae53d50192d1ea
Snaymi <sneymeb@hotmail.com>
2023-03-26 21:07:45 -0300

    Finalizado

---
+++ MesadaSobrinho.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MesadaSobrinho {
+    public static void main (String [] args){
+        Scanner scan = new Scanner(System.in);
+        //Variáveis
+        int money = 50;
+        int mes;
+        int total;
+        String resp;
+        
+        
+    
+
+         
+        
+        mes = scan.nextInt();
+        
+        total = money * mes;
+        System.out.print(total);
+            
+            
+
+    }
+}
